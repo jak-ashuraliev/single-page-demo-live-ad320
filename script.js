90b034554e9df9e650c37dc511d7d7651421ab80
Jak Ashuraliev <ashuraliev@gmail.com>
2020-01-23 18:31:01 -0800

    Update js function

--- script.js
+++ script.js
@@ -3,16 +3,3 @@ document.getElementById('display').addEventListener('click', function(){
     console.log(date);
     alert('Current Date And Time: ' + date.toUTCString());
 });
-
-
-// Another way:
-/*
-
-function showDate() {
-    let timeStamp = new Date();
-    document.getElementById('display');
-    console.log(timeStamp.toUTCString());
-    alert(timeStamp.toUTCString());
-}
-
-*/
